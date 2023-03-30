c0b9d8eed3317bf3a0f6a4cefdda9bfd2b0fa349
pk381 <114735272+pk381@users.noreply.github.com>
2023-03-30 11:25:59 +0530

    Create index.php

---
+++ index.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Web project</title>
+  <link rel="stylesheet" href="Style/style.css">
+</head>
+<body>
+  <div class="menu-bar">
+  <ul>
+      <li><a href ="index.php"> Home</a></li>
+      <li><a href = "members.php">Members</a></li>
+      <li><a href = "resource.php" >Resources</a></li>
+      <li><a href = "contacts.php">Contact</a></li>
+  </ul>
+</div>
+
+  <div id = "box">
+
+    <div>
+
+      <h1>NITC RESUME BUILDER</h1>
+      
+      
+        <p class="text"> A website that helps NITC students to build their resume in an easy<span class="dots">...</span><span class="moretext">
+        and efficient manner . After filling their details they will get resume in just seconds.</span></p>
+        <button class="read-more-btn">Read More</button>
+        <script src="main.js"></script>
+    </div>
+
+
+  <div id="reg">
+    
+      <a href="reg.php">Register/Login</a>
+  </div>
+</div>
+
+</body>
+</html>
